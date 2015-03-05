0a131b53dcab8eb9a8191738863429417c40d1c3
Allan Ebdrup <aeb@e-conomic.com>
2015-03-05 09:57:27 +0100

    support for ready branches with no pull request

--- debitoor.sh
+++ debitoor.sh
@@ -28,64 +28,75 @@ delete_ready_branch (){
 	exit $1
 }
 
-
-
-################################################
-# Make sure git fetches (hidden) Pull Requests
-# by adding:
-# fetch = +refs/pull/*/head:refs/remotes/origin/pullrequest/*
-# to .git/config under the origin remote
-################################################
-
-step_start "Adding fetch of pull requests to .git/config"
-
-CURRENT_FETCH=`grep '	fetch =.\+refs/pull/\*/head:refs/remotes/origin/pullrequest/\*' .git/config`
-if [ "$CURRENT_FETCH" = '' ]
-then
-	sed -i 's/\[remote \"origin\"\]/[remote "origin"]\
-	fetch = +refs\/pull\/*\/head:refs\/remotes\/origin\/pullrequest\/*/g' .git/config
-	echo 'Added fetch of pull request to .git/config:'
-	cat .git/config
-else
-	echo 'Fetch of pull request already in place in .git/config'
-fi
-git fetch --prune || delete_ready_branch $?
-
-########################################################################################
-# Lookup PR number
-# By looking the SHA checksum of the current branchs latests commit
-# And finding a pull request that has a matching SHA checksum as the lastest commit
-# This enforces a restriction that you can only merge branches that match a pull request
-# And using the number of the pull request later, we can close the pull request
-# by making the squash merge commit message include "fixes #[pull request number] ..."
-########################################################################################
-
-step_start "Finding pull request that matches current branch"
-
-CURRENT_SHA=`git log -1 --format="%H"`
-echo "Current SHA:"
-echo "${CURRENT_SHA}"
+step_start "Finding author"
 
 LAST_COMMIT_AUTHOR=`git log --pretty=format:'%an' -n 1`
 echo "This will be the author of the merge commit in master: ${LAST_COMMIT_AUTHOR} (the last commit in branch was done by this person)"
 
-error='\nDid you try to deploy a branch that is not a pull request?\nOr did you forget to push your changes to github?'
 
-MATCHING_PULL_REQUEST=`git show-ref | grep $CURRENT_SHA | grep 'refs/remotes/origin/pullrequest/'`
-if [ "$MATCHING_PULL_REQUEST" = '' ] ; then
-  echo "Error finding matching pull request: ${error}" >&2; delete_ready_branch 1
-fi
-echo "Matching pull request:"
-echo "${MATCHING_PULL_REQUEST}"
+case ${branch} in
+*_no_pull_request)
+  ## If the ready branch ends with "_no_pull_request" we will not try to match to a pull request. This is for merging latest texts
+  step_start "No pull request - Git fetching"
+  PR_NUMBER="none"
+  git fetch --prune || delete_ready_branch $?
+  ;;
+*)
+	################################################
+	# Make sure git fetches (hidden) Pull Requests
+	# by adding:
+	# fetch = +refs/pull/*/head:refs/remotes/origin/pullrequest/*
+	# to .git/config under the origin remote
+	################################################
 
-PR_NUMBER=`echo "${MATCHING_PULL_REQUEST}" | sed 's/[0-9a-z]* refs\/remotes\/origin\/pullrequest\///g' | sed 's/\s//g'`
-echo "Extracted pull request number:"
-echo "${PR_NUMBER}"
-case ${PR_NUMBER} in
-    ''|*[!0-9]*) echo "Error pull request number does not match number regExp (weird!): ${error}" >&2; delete_ready_branch 1 ;;
-    *) echo "Success. Pull request number passes regExp test for number. Exporting PR_NUMBER=${PR_NUMBER}" ;;
+	step_start "Adding fetch of pull requests to .git/config"
+
+	CURRENT_FETCH=`grep '	fetch =.\+refs/pull/\*/head:refs/remotes/origin/pullrequest/\*' .git/config`
+	if [ "$CURRENT_FETCH" = '' ]
+	then
+		sed -i 's/\[remote \"origin\"\]/[remote "origin"]\
+		fetch = +refs\/pull\/*\/head:refs\/remotes\/origin\/pullrequest\/*/g' .git/config
+		echo 'Added fetch of pull request to .git/config:'
+		cat .git/config
+	else
+		echo 'Fetch of pull request already in place in .git/config'
+	fi
+	git fetch --prune || delete_ready_branch $?
+
+	########################################################################################
+	# Lookup PR number
+	# By looking the SHA checksum of the current branchs latests commit
+	# And finding a pull request that has a matching SHA checksum as the lastest commit
+	# This enforces a restriction that you can only merge branches that match a pull request
+	# And using the number of the pull request later, we can close the pull request
+	# by making the squash merge commit message include "fixes #[pull request number] ..."
+	########################################################################################
+
+	step_start "Finding pull request that matches current branch"
+
+	CURRENT_SHA=`git log -1 --format="%H"`
+	echo "Current SHA:"
+	echo "${CURRENT_SHA}"
+
+
+	error='\nDid you try to deploy a branch that is not a pull request?\nOr did you forget to push your changes to github?'
+
+	MATCHING_PULL_REQUEST=`git show-ref | grep $CURRENT_SHA | grep 'refs/remotes/origin/pullrequest/'`
+	if [ "$MATCHING_PULL_REQUEST" = '' ] ; then
+	  echo "Error finding matching pull request: ${error}" >&2; delete_ready_branch 1
+	fi
+	echo "Matching pull request:"
+	echo "${MATCHING_PULL_REQUEST}"
+
+	PR_NUMBER=`echo "${MATCHING_PULL_REQUEST}" | sed 's/[0-9a-z]* refs\/remotes\/origin\/pullrequest\///g' | sed 's/\s//g'`
+	echo "Extracted pull request number:"
+	echo "${PR_NUMBER}"
+	case ${PR_NUMBER} in
+		*[!0-9]*) echo "Error pull request number does not match number regExp (weird!): ${error}" >&2; delete_ready_branch 1 ;;
+		*) echo "Success. Pull request number passes regExp test for number. Exporting PR_NUMBER=${PR_NUMBER}" ;;
+	esac
+	;;
 esac
-
 #####################################################################
 # Checkout master
 # Cleanup any leftovers for previous failed merges (reset --hard)
@@ -114,7 +125,12 @@ git config user.email "teamcity@e-conomic.com" || delete_ready_branch $?
 git config user.name "Teamcity" || delete_ready_branch $?
 git merge --squash "origin/ready/${branch}" || delete_ready_branch $?
 branchWithUnderscore2SpacesAndRemovedTimestamp=`echo "${branch}" | sed -e 's/_/ /g' | sed -e 's/\/[0-9]*s$//g'`
-commitMessage="fixes #${PR_NUMBER} - ${branchWithUnderscore2SpacesAndRemovedTimestamp}"
+if [ "PR_NUMBER" = 'none' ]
+then
+	commitMessage="${branchWithUnderscore2SpacesAndRemovedTimestamp}"
+felse
+	commitMessage="fixes #${PR_NUMBER} - ${branchWithUnderscore2SpacesAndRemovedTimestamp}"
+fi
 echo "Committing squashed merge with message: \"${message}\""
 git commit -m "${commitMessage}" --author "${LAST_COMMIT_AUTHOR}" || message_on_commit_error $?
 
