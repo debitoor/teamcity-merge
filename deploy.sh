2eec1abe905c1a339e0cea4344e0d16b90050f8c
Allan Ebdrup <aeb@e-conomic.com>
2015-04-02 14:22:17 +0200

    minor cleanup

--- deploy.sh
+++ deploy.sh
@@ -28,7 +28,7 @@ if [ "$branch" = 'refs/heads/master' ]
 then
     echo "Master branch"
 else
-    echo "Error not master branch" >&2
+    echo "Error: Not master branch" >&2
     _exit 1
 fi
 
@@ -42,7 +42,7 @@ if [ "$comitter" = 'Teamcity' ]
 then
     echo "Latest commit to master is by Teamcity"
 else
-    echo "Error. Latest commit to master is NOT by Teamcity" >&2
+    echo "Error: Latest commit to master is NOT by Teamcity" >&2
     _exit 1
 fi
 
@@ -54,7 +54,7 @@ step_start "Checking that latest commit has no tag. If it has a tag it is alread
 returnValueWhenGettingTag=`git describe --exact-match --abbrev=0 &> /dev/null; echo $?`
 if [ "$returnValueWhenGettingTag" = '0' ]
 then
-    echo "Master already has a tag, it is already deployed, skipping deploy"
+    echo "Master already has a tag, it is already deployed. Skipping deploy"
     _exit 0
 else
     echo "Master has no tag yet, lets deploy"
@@ -69,20 +69,20 @@ project=`cat package.json | grep "\"name\": \"" | sed 's/\s*"name": "//g' | sed 
 hms deploy production-services "${project}" --no-log --retry || _exit $?
 
 ################################################
-# Add git tag and push to github
+# Add git tag and push to GitHub
 ################################################
 
-step_start "Adding git tag and pushing to github"
+step_start "Adding git tag and pushing to GitHub"
 datetime=`date +%Y-%m-%d_%H-%M-%S`
 commitMessage=`git log -1 --pretty=%B`
 git tag -a "${project}.production.${datetime}" -m "${commitMessage}" || _exit $?
 git push origin --tags || _exit $?
 
 ################################################
-# Mark deploy on new relic
+# Mark deploy on New Relic
 ################################################
 
-step_start "Tagging deploy on new relic"
+step_start "Marking deploy on New Relic"
 author=`git log --pretty=format:'%an' -n 1`
 curl -H "x-api-key:${NEW_RELIC_API_KEY}" -d "deployment[app_name]=${project}" -d "deployment[user]=${author}" -d "deployment[description]=${commitMessage}" https://api.newrelic.com/deployments.xml || _exit $?
 _exit 0
