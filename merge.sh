31304e97fb4837e4b6f641b78872bedd1aeca615
Allan Ebdrup <aeb@e-conomic.com>
2015-04-09 10:49:18 +0200

    give step a name

--- merge.sh
+++ merge.sh
@@ -122,6 +122,8 @@ esac
 #####################################################################
 # Merge latests texts to ready branch
 #####################################################################
+
+step_start "Merge latests texts to ready branch"
 git fetch origin texts || delete_ready_branch $? "Could not fetch texts branch"
 git config user.email "teamcity@e-conomic.com" || delete_ready_branch $? "Could not set git email"
 git config user.name "Teamcity" || delete_ready_branch $? "Could not set git user name"
