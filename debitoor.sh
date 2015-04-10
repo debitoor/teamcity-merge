63face70071b001d59dd2274b33541081d47193b
Allan Ebdrup <aeb@e-conomic.com>
2015-04-10 15:55:41 +0200

    update debitoor.sh with latest changes

--- debitoor.sh
+++ debitoor.sh
@@ -19,18 +19,44 @@ step_start(){
 	echo "##teamcity[blockOpened name='${stepName}']"
 }
 
+hipchat(){
+	curl -H "Content-Type: application/json" \
+		 -X POST \
+		 -d "{\"color\": \"$2\", \"notify\": \"true\", \"message_format\": \"text\", \"message\": \"$1\" }" \
+		 "https://api.hipchat.com/v2/room/807962/notification?auth_token=${HIPCHAT_API_KEY}"
+}
 
-# Always last thing done before exit
+# Always last thing done before deploy
 delete_ready_branch (){
 	step_start "Deleting ready branch on github"
 	git push origin ":ready/${branch}"
 	step_end
-	exit $1
+	hipchatUser=`echo "${LAST_COMMIT_AUTHOR}" | sed 's/\s//g'`
+	if [ "$1" = '0' ]
+	then
+		if [ "$2" != '' ]
+		then
+			hipchat "$2: ${project}\n@${hipchatUser}\n${commitMessage}" green
+			echo "$2: ${project}\n@${hipchatUser}\n${commitMessage}"
+		else
+			hipchat "Success merging: ${project}\n@${hipchatUser}\n${commitMessage}" green
+			echo "Success merging: ${project}\n@${hipchatUser}\n${commitMessage}"
+		fi
+		deploy
+	else
+		hipchat "Failure merging: $2 - ${project}\n@${hipchatUser}\n${commitMessage}" red
+		echo "Failure merging: $2 - ${project}\n@${hipchatUser}\n${commitMessage}"
+	fi
 }
+project=`cat package.json | grep "\"name\": \"" | sed 's/\s*"name": "//g' | sed 's/"//g' | sed 's/,//g' | sed 's/\s//g'`
+commitMessage="${branch}"
+git config user.email "teamcity@e-conomic.com" || delete_ready_branch $? "Could not set git email"
+git config user.name "Teamcity" || delete_ready_branch $? "Could not set git user name"
 
 step_start "Finding author"
 
 LAST_COMMIT_AUTHOR=`git log --pretty=format:'%an' -n 1`
+
 echo "This will be the author of the merge commit in master: ${LAST_COMMIT_AUTHOR} (the last commit in branch was done by this person)"
 
 
@@ -39,7 +65,7 @@ case ${branch} in
   ## If the ready branch ends with "_no_pull_request" we will not try to match to a pull request. This is for merging latest texts
   step_start "No pull request - Git fetching"
   PR_NUMBER="none"
-  git fetch --prune || delete_ready_branch $?
+  git fetch --prune || delete_ready_branch $? "Could not git fetch"
   ;;
 *)
 	################################################
@@ -61,7 +87,7 @@ case ${branch} in
 	else
 		echo 'Fetch of pull request already in place in .git/config'
 	fi
-	git fetch --prune || delete_ready_branch $?
+	git fetch --prune || delete_ready_branch $? "Could not git fetch"
 
 	########################################################################################
 	# Lookup PR number
@@ -83,7 +109,7 @@ case ${branch} in
 
 	MATCHING_PULL_REQUEST=`git show-ref | grep $CURRENT_SHA | grep 'refs/remotes/origin/pullrequest/'`
 	if [ "$MATCHING_PULL_REQUEST" = '' ] ; then
-	  echo "Error finding matching pull request: ${error}" >&2; delete_ready_branch 1
+	  echo "Error finding matching pull request: ${error}" >&2; delete_ready_branch 1 "Could not find matching pull request"
 	fi
 	echo "Matching pull request:"
 	echo "${MATCHING_PULL_REQUEST}"
@@ -92,47 +118,85 @@ case ${branch} in
 	echo "Extracted pull request number:"
 	echo "${PR_NUMBER}"
 	case ${PR_NUMBER} in
-		''|*[!0-9]*) echo "Error pull request number does not match number regExp (weird!): ${error}" >&2; delete_ready_branch 1 ;;
+		''|*[!0-9]*) echo "Error pull request number does not match number regExp (weird!): ${error}" >&2; delete_ready_branch 1 "Could not find pull request number";;
 		*) echo "Success. Pull request number passes regExp test for number. Exporting PR_NUMBER=${PR_NUMBER}" ;;
 	esac
 	;;
 esac
+
+#####################################################################
+# Merge latests texts to ready branch
+#####################################################################
+
+if [ "$1" = 'texts' ]
+then
+	step_start "Merge latests texts to ready branch"
+
+	git fetch origin texts || delete_ready_branch $? "Could not fetch texts branch"
+	git config merge.renamelimit 999999 || delete_ready_branch $? "Could not set git renamelimit"
+	git merge origin/texts --squash -X theirs || delete_ready_branch $? "Could not merge latest texts"
+	git checkout source/texts/translations.json --theirs || delete_ready_branch $? "Could not checkout translations.json"
+	git add source/texts/translations.json || delete_ready_branch $? "Could not git add translations.json"
+	git commit -m 'merged latest texts' || echo "ignoring nothing to commit, continuing"
+fi
+
 #####################################################################
 # Checkout master
-# Cleanup any leftovers for previous failed merges (reset --hard)
+# Cleanup any leftovers for previous failed merges (reset --hard, clean -fx)
 # And pull master
 #####################################################################
 
-step_start "Checking out, resetting (hard) and pulling master branch"
+step_start "Checking out master, resetting (hard), pulling from origin and cleaning"
 
-git checkout master || delete_ready_branch $?
-git reset --hard origin/master || delete_ready_branch $?
-git pull || delete_ready_branch $?
+git checkout master || delete_ready_branch $?  "Could not checkout master"
+git reset --hard origin/master || delete_ready_branch $? "Could not reset to master"
+git pull || delete_ready_branch $? "Could not pull master"
+git clean -fx || delete_ready_branch $? "Could not git clean on master"
+
+#####################################################################
+# Merge latests texts to master branch
+#####################################################################
+
+if [ "$1" = 'texts' ]
+then
+	step_start "Merge latests texts to master branch"
+
+	git merge origin/texts --squash -X theirs || delete_ready_branch $? "Could not merge latest texts to master"
+	git checkout source/texts/translations.json --theirs || delete_ready_branch $? "Could not checkout translations.json (master)"
+	git add source/texts/translations.json || delete_ready_branch $? "Could not git add translations.json to master"
+	git commit -m 'merged latest texts' || echo "ignoring nothing to commit, continuing"
+fi
 
 ################################################
 # Merge into master
 # You will want to use you own email here
 ################################################
 
-step_start "Merging ready branch into master, with commit message that closes pull request number ${PR_NUMBER}"
+case ${branch} in
+merge_latest_texts*)
+	step_start "Branch name starts with merge_latest_texts - skipping merging ready branch into master"
+  	;;
+*)
+	step_start "Merging ready branch into master, with commit message that closes pull request number ${PR_NUMBER}"
 
-message_on_commit_error(){
-	commitErrorCode=$1
-	echo 'Commiting changes returned an error (status: ${commitErrorCode}). We are assuming that this is due to no changes, and continuing'
-}
+	message_on_commit_error(){
+		commitErrorCode=$1
+		echo 'Commiting changes returned an error (status: ${commitErrorCode}). We are assuming that this is due to no changes, and exiting gracefully'
+		delete_ready_branch 0 "No changes in ready build"
+	}
 
-git config user.email "teamcity@e-conomic.com" || delete_ready_branch $?
-git config user.name "Teamcity" || delete_ready_branch $?
-git merge --squash "origin/ready/${branch}" || delete_ready_branch $?
-branchWithUnderscore2SpacesAndRemovedTimestamp=`echo "${branch}" | sed -e 's/_/ /g' | sed -e 's/\/[0-9]*s$//g'`
-if [ "$PR_NUMBER" = 'none' ]
-then
-	commitMessage="${branchWithUnderscore2SpacesAndRemovedTimestamp}"
-else
-	commitMessage="fixes #${PR_NUMBER} - ${branchWithUnderscore2SpacesAndRemovedTimestamp}"
-fi
-echo "Committing squashed merge with message: \"${message}\""
-git commit -m "${commitMessage}" --author "${LAST_COMMIT_AUTHOR}" || message_on_commit_error $?
+	git merge --squash "ready/${branch}" || delete_ready_branch $? "Merge conflicts (could not merge)"
+	branchWithUnderscore2SpacesAndRemovedTimestamp=`echo "${branch}" | sed -e 's/_/ /g' | sed -e 's/\/[0-9]*s$//g'`
+	if [ "$PR_NUMBER" = 'none' ]
+	then
+		commitMessage="${branchWithUnderscore2SpacesAndRemovedTimestamp}"
+	else
+		commitMessage="fixes #${PR_NUMBER} - ${branchWithUnderscore2SpacesAndRemovedTimestamp}"
+	fi
+	echo "Committing squashed merge with message: \"${message}\""
+	git commit -m "${commitMessage}" --author "${LAST_COMMIT_AUTHOR}" || message_on_commit_error $?
+	;;
+esac
 
 ################################################
 # Run tests
@@ -140,7 +204,7 @@ git commit -m "${commitMessage}" --author "${LAST_COMMIT_AUTHOR}" || message_on_
 
 step_start "Running tests with >npm run teamcity "
 
-npm run teamcity || delete_ready_branch $?
+npm run teamcity || delete_ready_branch $? "Failing test(s)"
 
 ################################################
 # Push changes to github
@@ -148,55 +212,46 @@ npm run teamcity || delete_ready_branch $?
 
 step_start "Pushing changes to github master branch"
 
-git push origin master || delete_ready_branch $?
+git push origin master || delete_ready_branch $? "Could not push changes to GitHub"
 
-################################################
-# Deploy to production
-################################################
+delete_ready_branch 0
 
-project=`cat package.json | grep "\"name\": \"" | sed 's/\s*"name": "//g' | sed 's/"//g' | sed 's/,//g' | sed 's/\s//g'`
-if [ "$1" = 'deploy' ]
-then
+_exit (){
+	step_end
+	exit $1
+}
+
+
+deploy(){
+	################################################
+	# Deploy to production
+	################################################
+
 	step_start "Deploying to production"
-	hms deploy production-services "${project}" --no-log --retry || delete_ready_branch $?
-else
-	step_start "Skipping deploy to production"
-	echo "No deploy - to deploy with hms, please pass \"deploy\" parameter to this script:"
-	echo "cat debitoor.sh | sh -s deploy"
-fi
+	commitMessage=`git log -1 --pretty=%B`
+	LAST_COMMIT_AUTHOR=`git log --pretty=format:'%an' -n 1`
+	project=`cat package.json | grep "\"name\": \"" | sed 's/\s*"name": "//g' | sed 's/"//g' | sed 's/,//g' | sed 's/\s//g'`
+	hms deploy production-services "${project}" --no-log --retry || _exit $?
+	hipchatUser=`echo "${LAST_COMMIT_AUTHOR}" | sed 's/\s//g'`
+	hipchat "Success deploying ${project}\n@${hipchatUser}\n${commitMessage}" green
 
-################################################
-# Mark deploy on new relic
-################################################
+	################################################
+	# Add git tag and push to GitHub
+	################################################
 
-if [ "$1" = 'deploy' ]
-then
-	step_start "Tagging deploy on new relic"
-	curl -H "x-api-key:${NEW_RELIC_API_KEY}" -d "deployment[app_name]=${project}" -d "deployment[user]=${LAST_COMMIT_AUTHOR}" -d "deployment[description]=${commitMessage}" https://api.newrelic.com/deployments.xml || delete_ready_branch $?
-else
-	step_start "Skipping tagging deploy on new relic"
-	echo "No deploy - to deploy with hms, please pass \"deploy\" parameter to this script:"
-	echo "cat debitoor.sh | sh -s deploy"
-fi
-
-################################################
-# Add git tag a nd push to github
-################################################
-
-if [ "$1" = 'deploy' ]
-then
-	step_start "Adding git tag and pushing to github"
+	step_start "Adding git tag and pushing to GitHub"
+	git config user.email "teamcity@e-conomic.com" || _exit $?
+	git config user.name "Teamcity" || _exit $?
 	datetime=`date +%Y-%m-%d_%H-%M-%S`
-	git tag -a "${project}.production.${datetime}" -m "${commitMessage}" || delete_ready_branch $?
-	git push origin --tags || delete_ready_branch $?
-else
-	step_start "Skipping adding git tag"
-	echo "No deploy - to deploy with hms, please pass \"deploy\" parameter to this script:"
-	echo "cat debitoor.sh | sh -s deploy"
-fi
+	git tag -a "${project}.production.${datetime}" -m "${commitMessage}" || _exit $?
+	git push origin --tags || _exit $?
 
-################################################
-# Delete the ready branch
-################################################
+	################################################
+	# Mark deploy on New Relic
+	################################################
 
-delete_ready_branch
+	step_start "Marking deploy on New Relic"
+	author=`git log --pretty=format:'%an' -n 1`
+	curl -H "x-api-key:${NEW_RELIC_API_KEY}" -d "deployment[app_name]=${project}" -d "deployment[user]=${author}" -d "deployment[description]=${commitMessage}" https://api.newrelic.com/deployments.xml || _exit $?
+	_exit 0
+}
