620ad9ef83ba67dd4fde3ec424a6cf6162f1da47
Allan Ebdrup <allan@878.dk>
2017-04-27 10:13:28 +0200

    handle teamcity commits

--- getSlackUser.sh
+++ getSlackUser.sh
@@ -24,15 +24,20 @@ getSlackName(){
 function getSlackUser()
 {
 	email=`git log --pretty=format:'%ae' -n 1`
-	slackUser=`getSlackName "${email}"`
-	users=`curl -X POST "https://slack.com/api/users.list?token=${SLACK_TOKEN}" -s`
-	slackUserId=`node -e "let users =  ${users}; console.log(users.members.reduce((acc,m)=>{ if(m.name==='${slackUser}'||m.profile.email==='${email}'){return m.id} return acc;}, ''))"`
-	slackUser=`node -e "let users =  ${users}; console.log(users.members.reduce((acc,m)=>{ if(m.name==='${slackUser}'||m.profile.email==='${email}'){return m.name} return acc;}, ''))"`
-	if [ "${slackUserId}" = '' ]
+	if [ "${email}" = 'debitoor-bot@debitoor.com' ]
 	then
-		echo "${slackUser}|${email} (could not find this slack-username or email in slack?! Fix it here: https://github.com/debitoor/teamcity-merge/blob/master/getSlackUser.sh)"
+		echo "TeamCity"
 	else
-		echo "<${slackUserId}|${slackUser}>"
+		slackUser=`getSlackName "${email}"`
+		users=`curl -X POST "https://slack.com/api/users.list?token=${SLACK_TOKEN}" -s`
+		slackUserId=`node -e "let users =  ${users}; console.log(users.members.reduce((acc,m)=>{ if(m.name==='${slackUser}'||m.profile.email==='${email}'){return m.id} return acc;}, ''))"`
+		slackUser=`node -e "let users =  ${users}; console.log(users.members.reduce((acc,m)=>{ if(m.name==='${slackUser}'||m.profile.email==='${email}'){return m.name} return acc;}, ''))"`
+		if [ "${slackUserId}" = '' ]
+		then
+			echo "${slackUser}|${email} (could not find this slack-username or email in slack?! Fix it here: https://github.com/debitoor/teamcity-merge/blob/master/getSlackUser.sh)"
+		else
+			echo "<${slackUserId}|${slackUser}>"
+		fi
 	fi
 }
 
