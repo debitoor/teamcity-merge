7d406f5e9e161ceced21c61df9c089a62a4ce8a7
Allan Ebdrup <allan@878.dk>
2017-04-29 10:38:13 +0200

    preserve linebreaks

--- debitoor.sh
+++ debitoor.sh
@@ -344,7 +344,7 @@ const path = require('path');
 let log = fs.readFileSync(path.join(__dirname, 'err.log'), 'utf-8')
 	.split('\n')
 	.filter(line => !/^npm (ERR|WARN)/.test(line))
-	.join('')
+	.join('\n')
 	.replace(/\|n/g, '\n');
 console.log(log);
 " && rm -f err.log`
